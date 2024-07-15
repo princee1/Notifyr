83578dcc2e798ac23c722ac13cd4a989f821db22
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-15 16:46:09 -0400

    added key to the configService

--- microservice/services/configService.py
+++ microservice/services/configService.py
@@ -7,7 +7,26 @@ class MODE(Enum):
     DEV_MODE = 'dev'
     PROD_MODE = 'prod'
     TEST_MODE = 'test'
-
-
+    
+    def toMode(val):
+        match val:
+            case MODE.DEV_MODE.value:
+                return MODE.DEV_MODE
+            case MODE.PROD_MODE.value:
+                return MODE.PROD_MODE
+            case MODE.TEST_MODE:
+                return MODE.TEST_MODE.value
+            case _:
+                return MODE.DEV_MODE
+            
 class ConfigService():
-    pass
+    ENV = ".env"
+    def __init__(self) -> None:
+        load_dotenv(ConfigService.ENV)
+        self.MODE = MODE.toMode(os.getenv('MODE'))
+        self.PORT = os.getenv("PORT")
+        self.LOG_LEVEL = os.getenv("LOG_LEVEL")
+        self.EMAIL_HOST = os.getenv("EMAIL_HOST").capitalize()
+        self.EMAIL_PORT = os.getenv("EMAIL_PORT")
+        self.EMAIL = os.getenv("EMAIL")
+        self.EMAIL_PASS = os.getenv("EMAIL_PASS")
