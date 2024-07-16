11d5aaa7b034b36937222b701e38faed0fbc944a
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-16 00:18:45 -0400

    added some try catch to the config service

--- microservice/services/configService.py
+++ microservice/services/configService.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from enum import Enum
+from __init__ import Service
 
 
 class MODE(Enum):
@@ -19,14 +20,30 @@ class MODE(Enum):
             case _:
                 return MODE.DEV_MODE
             
-class ConfigService():
+class ConfigService(Service):
     ENV = ".env"
     def __init__(self) -> None:
         load_dotenv(ConfigService.ENV)
         self.MODE = MODE.toMode(os.getenv('MODE'))
         self.PORT = os.getenv("PORT")
         self.LOG_LEVEL = os.getenv("LOG_LEVEL")
-        self.EMAIL_HOST = os.getenv("EMAIL_HOST").capitalize()
-        self.EMAIL_PORT = os.getenv("EMAIL_PORT")
+        self.EMAIL_HOST = os.getenv("EMAIL_HOST").upper()
+        try:
+            val = int(os.getenv("EMAIL_PORT"))
+            self.EMAIL_PORT = val
+        except:
+
+            self.EMAIL_PORT = None
+            pass
         self.EMAIL = os.getenv("EMAIL")
         self.EMAIL_PASS = os.getenv("EMAIL_PASS")
+        self.EMAIL_CONN_METHOD= os.getenv("EMAIL_CONN_METHOD")
+        try: 
+            self.EMAIL_LOG_LEVEL= int(os.getenv("EMAIL_LOG_LEVEL"))
+        except:
+            self.EMAIL_LOG_LEVEL = 0
+            pass
+
+
+    def buildService(self):
+        pass
