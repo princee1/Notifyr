78ee34fc54bf2acf8228a589b4beb6aabd4d8f32
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-23 00:41:29 -0400

    added signature for the assets services

---
+++ app/services/assetsService.py
@@ -0,0 +1,21 @@
+from module import Module
+class AssetServices(Module):
+    def __init__(self) -> None:
+        self.images = {}
+        self.htmls = {}
+        self.pdf = {}
+        pass
+
+    def build(self): pass
+    
+    def kill(self): pass
+
+    def readHtml(self): pass
+
+    def readPdf(self): pass
+
+    def readImages(self): pass
+
+    def loadCss(self): pass
+    
+    pass
