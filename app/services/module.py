628919060cdd10d93ea03ae776d34cbb4d413c52
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-23 11:10:09 -0400

    added some error class when building the dependency

--- app/services/module.py
+++ app/services/module.py
@@ -1,14 +1,26 @@
 from enum import Enum
 
 class BuildErrorLevel(Enum):
-    
-    pass
+    FAILURE = 4
+    ABORT = 3
+    WARNING = 2
+    SKIP = 1
 
 
 class BuildError(BaseException):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
     pass
+
+
+class BuildFailureError(BuildError): pass
+
+class BuildAbortError(BuildError): pass
+
+class BuildWarningError(BuildError): pass
+
+class BuildSkipError(BuildError): pass
+
 
 class Module():
 
@@ -18,19 +30,38 @@ class Module():
     def kill(self):
         pass
 
+    def log(self):
+        pass
+
     def __builder(self):
         try:
             self.build()
+        except BuildFailureError as e:
             pass
-        except:
+
+        except BuildAbortError as e:
             pass
-        pass
+
+        except BuildWarningError as e:
+            pass
+
+        except BuildSkipError as e:
+            pass
 
     def __killer(self):
         try:
             self.kill()
             pass
-        except:
+        except BuildFailureError as e:
+            pass
+
+        except BuildAbortError as e:
+            pass
+
+        except BuildWarningError as e:
+            pass
+
+        except BuildSkipError as e:
             pass
         pass
 
