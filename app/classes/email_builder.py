3b8a2a5bb5fb598ac11ef0b12a698b6714705b60
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-23 11:12:46 -0400

    renamed the directory and added a  email builder

---
+++ app/classes/email_builder.py
@@ -0,0 +1,10 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+
+class MessageBuilder():
+    def __init__(self,subject:str,From:str,To: str|list[str],body:str, CC:str,) -> None:
+        
+        pass
+    pass
