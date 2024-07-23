16c0942c9726130206fa3791c319d1f3722b3784
princeey1 <prince-david.madzou@polymtl.ca>
2024-07-23 11:15:43 -0400

    added the services in the dependency file

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,13 @@
 from services.assetsService import AssetService
 from services.configService import ConfigService
-from services.emailService import EmailReader,EmailSender
+from services.emailService import EmailReader, EmailSender
 from services.twilloService import TwilioService
+from services.supportService import SupportService, ChatService
+from services.communicationService import PhoneService, SMSService
+from services.trainingService import TrainingService
 
-__DEPENDENCY: list[type] = [AssetService, ConfigService, EmailReader, EmailSender, TwilioService]
+
+
+__DEPENDENCY: list[type] = [AssetService, ConfigService, EmailReader, EmailSender,
+                            TwilioService, SupportService, ChatService, PhoneService, TrainingService,  SMSService]
+
