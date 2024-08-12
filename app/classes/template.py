594e247e9bf4115c0604537980608bcab730ae08
princeey1 <prince-david.madzou@polymtl.ca>
2024-08-11 22:08:07 -0400

    added a reader object that reads and store and create an Asset / Template object (which is essentially a file ) based on the parameter passed

--- app/classes/template.py
+++ app/classes/template.py
@@ -1,43 +1,75 @@
 
-from dataclasses import dataclass
+from enum import Enum
+from typing import Any
 from bs4 import BeautifulSoup
 import fitz as pdf
 from googletrans import Translator
+import os
 
-@dataclass
-class Template():
-    content: str | bytes
-    filename: str
-    dirName:str
-    keys: set[str]
-    lang:str
-    def __init__(self,filename:str,content:str,dirName:str,lang:str) -> None:
+
+class Parser(Enum):
+    HTML= "html.parser"
+    LXML = "lxml"
+
+
+ROUTE_SEP= "-"
+
+
+class Asset():
+    def __init__(self,filename:str,content:str,dirName:str) -> None:
+        super().__init__()
         self.filename = filename
         self.content = content
-        self.lang = lang
         self.dirName = dirName
+        self.name = self.filename.split(".")[0]
+    pass
+
+class Template(Asset):
+    LANG = None
+    def __init__(self,filename:str,content:str,dirName:str) -> None:
+        super().__init__(filename,content,dirName)
+        self.keys:list[str] = []
 
 
-    def inject(self, data: dict):pass
+    def inject(self, data:  dict):pass
     def load(self): pass
     def build(self): pass
     def translate(self): pass
-    def clone(self):pass
     def exportText(self):pass
 
+    @property
+    def routeName(self,): return self.name.replace(os.sep,ROUTE_SEP)
+
 class HTMLTemplate(Template):
-    def __init__(self, filename:str,content:str,dirName:str,lang:str) -> None:
-        super().__init__(filename, content,dirName,lang)
-
-    def loadCSS(self,css:str, priority:int):pass
-
-class PDFTemplate(Template):
-
-    def encrypt():pass
-
-    def decrypt():pass
+    def __init__(self, filename:str,content:str,dirName:str) -> None:
+        super().__init__(filename, content,dirName)
+        self.bs4 = BeautifulSoup(self.content,Parser.LXML.value)
+        self.validation:dict[str,str] = {}
 
 
-class SMSTemplate(Template):pass
+    def inject(self, data: dict):
+        return super().inject(data)
 
-class PhoneTemplate(Template): pass
+    def loadCSS(self,cssFiles: list[str]): pass
+
+
+    def loadImage(self,image): pass
+
+    def extractValidation(self,): pass
+
+class PDFTemplate(Template): 
+    def __init__(self, filename:str,content:str,dirName:str) -> None:
+        super().__init__(filename, content,dirName)
+
+
+    def encrypt(self, key:str):pass
+
+    def decrypt(self,key:str):pass
+
+class SMSTemplate(Template):
+    def __init__(self, filename:str,content:str,dirName:str) -> None:
+        super().__init__(filename, content,dirName)
+
+class PhoneTemplate(Template):
+    def __init__(self, filename:str,content:str,dirName:str) -> None:
+        super().__init__(filename, content,dirName)
--- app/services/assets.py
+++ app/services/assets.py
@@ -1,47 +1,88 @@
+from .config import ConfigService
+from utils.fileIO import FDFlag
+from classes.template import Asset, HTMLTemplate, PDFTemplate, SMSTemplate, PhoneTemplate, Template
 from .security import SecurityService
-from .file import FileService
+from .file import FileService, FTPService
 from . import _service
 from injector import inject
 from enum import Enum
+import os
+from threading import Thread
+from typing import Any
+
+ROOT_PATH = "assets/"
+def path(x): return ROOT_PATH+x
 
 
 class Extension(Enum):
-    HTML = ".html"
-    CSS = ".css"
-    SCSS = ".scss"
-    JPEG = ".jpg"
-    PDF = ".pdf"
-    TXT = ".txt"
+    HTML = "html"
+    CSS = "css"
+    SCSS = "scss"
+    JPEG = "jpg"
+    PDF = "pdf"
+    TXT = "txt"
+
+def extension(extension: Extension): return f".{extension.value}"
+
+class Reader():
+    def __init__(self, fileService: FileService, asset:type[Asset]) -> None:
+        self.fileService = fileService
+        self.asset = asset
+        self.values : dict[str,asset] = {}
+        
+    def read(self, ext: Extension, flag: FDFlag,rootFlag=False,encoding="utf-8"):
+        extension_ = extension(ext)
+        root = path(ext.value) if rootFlag else None
+        setTempFile:set[str]= {}  
+        for file in self.fileService.listFileExtensions(extension_, root, recursive=True):
+            relpath = root + os.path.sep+file
+            filename, content, dir = self.fileService.readFileDetail(relpath, flag,encoding)
+            keyName = filename if not setTempFile  else file
+            setTempFile.add(keyName)
+            self.values[relpath] = self.asset(keyName, content, dir)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        self.read(*args)
+        return self.values
 
 
+class ThreadedReader(Reader, Thread): pass # TODO threaded reader
+
+@_service.PossibleDep([FTPService])
 class AssetService(_service.Service):
     @inject
-    def __init__(self, fileService: FileService, securityService: SecurityService) -> None:
+    def __init__(self, fileService: FileService, securityService: SecurityService, configService: ConfigService) -> None:
         self.fileService = fileService
         self.securityService = securityService
-        self.images = {}
-        self.htmls = {}
-        self.pdf = {}
-        self.sms = {}
-        self.css = {}
+        self.configService = configService
+
+        self.images: dict[str, Asset] = {}
+        self.css: dict[str, Asset] = {}
+
+        self.htmls: dict[str, HTMLTemplate] = {}
+        self.pdf: dict[str, PDFTemplate] = {}
+        self.sms: dict[str, SMSTemplate] = {}
+        self.phone: dict[str, PhoneTemplate] = {}
         pass
 
-    def build(self): pass
+    def build(self):
+        self.readImages()
+        self.readHtml()
+        self.readPdf()
+        self.readSMS()
 
     def destroy(self): pass
 
     def readHtml(self): pass
-
+    
     def readPdf(self): pass
 
     def readImages(self): pass
 
     def readSMS(self): pass
 
-    def loadCss(self): pass
+    def readCSS(self): pass
 
     def encryptPdf(self): pass
 
     def decryptPdf(self): pass
-
-    pass
