003b0d050e31d6ddcec675bee83ae34559a90627
princeey1 <prince-david.madzou@polymtl.ca>
2024-12-26 00:41:53 -0500

    added a get user agent fast api dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,2 +1,7 @@
-def get_user_language(request):
-    pass
+from fastapi import Request
+
+def get_user_language(request: Request) -> str:
+    return ... 
+
+def get_user_agent(request: Request) -> str:
+    return ...
