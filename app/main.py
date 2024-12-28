b4c3cbaf0053914991ac9df46029862240c21ff5
princeey1 <prince-david.madzou@polymtl.ca>
2024-12-27 22:01:14 -0500

    updated the ui a bit

--- app/main.py
+++ app/main.py
@@ -1,27 +1,29 @@
+from server import AppParameter, start_applications, createApps, editApps, RESSOURCES, MIDDLEWARE
+from utils.fileIO import ConfigFile, JSONFile, exist, inputFilePath
 from argparse import ArgumentParser
-from utils.prettyprint import printJSON, show,PrettyPrinter_
+from utils.prettyprint import printJSON, show, PrettyPrinter_
 from enum import Enum
+
 
 class RunMode(Enum):
     FILE = "file"
     CREATE = "create"
     EDIT = "edit"
 
+
 parser = ArgumentParser(description="Communication Service Application")
 parser.add_argument('--mode', '-m', choices=['file', 'create', 'edit'],
                     default='file', type=str, help='Running Mode')
-parser.add_argument('--config', '-c', required=True,default=None,
+parser.add_argument('--config', '-c', required=True, default=None,
                     type=str, help='Path to the config file')
 args = parser.parse_args()
 
 mode = RunMode(args.mode)
 config_file = args.config
 
-show(1)
+PrettyPrinter_.show(1, print_stack=False)
 ########################################################################
 
-from utils.fileIO import ConfigFile, JSONFile, exist,inputFilePath
-from server import AppParameter,start_applications, createApps,editApps
 ########################################################################
 
 META_KEY = 'meta'
@@ -43,15 +45,16 @@ apps_data = config_json_app.data
 #     else:
 #         c_flag = False
 
-def app_params_to_json(params:list[AppParameter]): return [p.toJSON() for p in params]
+
+def app_params_to_json(params: list[AppParameter]): return {
+    META_KEY: {}, APPS_KEY: [p.toJSON() for p in params]}
 
 
-
-if  not apps_data or APPS_KEY not in apps_data or not apps_data[APPS_KEY] :
+if not apps_data or APPS_KEY not in apps_data or not apps_data[APPS_KEY]:
     mode = RunMode.CREATE
-    PrettyPrinter_.warning(f"Invalid config file: {config_file} - No apps data found")
+    PrettyPrinter_.warning(
+        f"Invalid config file: {config_file} - No apps data found")
     PrettyPrinter_.info(f"Running on CREATING mode")
-
 
 
 match mode:
@@ -59,20 +62,22 @@ match mode:
         apps_data = createApps()
         config_json_app.load(app_params_to_json(apps_data))
         PrettyPrinter_.success(f"Apps successfully created")
-        
+
     case RunMode.EDIT:
         apps_data = editApps(apps_data)
         config_json_app.load(app_params_to_json(apps_data))
         PrettyPrinter_.success(f"Apps successfully edited")
     case RunMode.FILE:
-        apps_data = [AppParameter.fromJSON(app) for app in apps_data[APPS_KEY]]
-        show(0.5)
+        apps_data = [AppParameter.fromJSON(
+            app, RESSOURCES, MIDDLEWARE) for app in apps_data[APPS_KEY]]
+        PrettyPrinter_.show(0, clear_stack=True)
         PrettyPrinter_.success(f"Apps Config successfully loaded")
     case _:
-        PrettyPrinter_.error(f"Erros while load apps")
+        PrettyPrinter_.error(f"Errors while load apps")
         exit(0)
 
-show(1)
-PrettyPrinter_.info('Starting applications ...')
+PrettyPrinter_.show(0, pause_before=1)
+PrettyPrinter_.info('Starting applications...')
+PrettyPrinter_.space_line()
 start_applications(apps_data)
 ########################################################################
