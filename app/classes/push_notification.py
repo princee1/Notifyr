0e90e3e6e9ca27a352f8b0a79859cb1278f948f6
princeey1 <prince-david.madzou@polymtl.ca>
2024-12-31 18:51:39 -0500

    fixed the conflicts method in requirements

--- app/classes/push_notification.py
+++ app/classes/push_notification.py
@@ -1,5 +1,5 @@
-from apns2.client import APNsClient
-from apns2.payload import Payload
+# from apns2.client import APNsClient
+# from apns2.payload import Payload
 
 from firebase_admin import credentials, messaging
 
