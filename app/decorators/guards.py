213c4b1dfbdc34f034ec18e8c996772edc7468c6
princeey1 <prince-david.madzou@polymtl.ca>
2025-01-13 02:21:55 -0500

    feat: add TwilioGuard and PlivoGuard classes for enhanced guard functionality

---
+++ app/decorators/guards.py
@@ -0,0 +1,11 @@
+from definition._utils_decorator import Guard
+from container import InjectInMethod
+from services.config_service import ConfigService
+from utils.constant  import HTTPHeaderConstant
+
+class TwilioGuard(Guard):
+    ...
+
+
+class PlivoGuard(Guard):
+    ...
