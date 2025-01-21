84ce1ae3b2d4015e046fbb7c52d2b9323dbbb110
princeey1 <prince-david.madzou@polymtl.ca>
2025-01-21 13:19:10 -0500

    feat: enhance middleware and config service with expiration handling for improved security

--- app/server/middleware.py
+++ app/server/middleware.py
@@ -1,24 +1,27 @@
+from fastapi.responses import JSONResponse
+from utils.constant import ConfigAppConstant
+from services.config_service import ConfigService
 from classes.permission import AuthPermission
 from services.security_service import SecurityService, JWTAuthService
 from container import InjectInMethod
-from fastapi import HTTPException, Request, Response, FastAPI,status
+from fastapi import HTTPException, Request, Response, FastAPI, status
 from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction
 from typing import Any, Awaitable, Callable, MutableMapping
 import time
 from interface.injectable_middleware import InjectableMiddlewareInterface
-from utils.dependencies import get_api_key, get_client_ip,get_bearer_token_from_request
+from utils.dependencies import get_api_key, get_client_ip, get_bearer_token_from_request
 from cryptography.fernet import InvalidToken
 from enum import Enum
 
 
-
-
 MIDDLEWARE: dict[str, type] = {}
+
 
 class MiddleWare(BaseHTTPMiddleware):
 
     def __init_subclass__(cls: type) -> None:
         MIDDLEWARE[cls.__name__] = cls
+
 
 class ProcessTimeMiddleWare(MiddleWare):
 
@@ -32,6 +35,7 @@ class ProcessTimeMiddleWare(MiddleWare):
         response.headers["X-Process-Time"] = str(process_time) + ' (s)'
         return response
 
+
 class SecurityMiddleWare(MiddleWare, InjectableMiddlewareInterface):
 
     def __init__(self, app, dispatch=None) -> None:
@@ -39,40 +43,45 @@ class SecurityMiddleWare(MiddleWare, InjectableMiddlewareInterface):
         InjectableMiddlewareInterface.__init__(self)
 
     async def dispatch(self, request: Request, call_next: Callable[..., Response]):
+        current_time = time.time()
+        if current_time - self.configService.config_json_app.data[ConfigAppConstant.META_KEY][ConfigAppConstant.EXPIRATION_TIMESTAMP_KEY] < 0:
+            return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "Unauthorized", "detail": "All Access and Auth token are expired"})
         try:
             request_api_key = get_api_key(request)
             if request_api_key is None:
                 return Response(status_code=status.HTTP_401_UNAUTHORIZED, content="Unauthorized")
             client_ip = get_client_ip(request)
-            if not self.securityService.verify_server_access(request_api_key,client_ip):
+            if not self.securityService.verify_server_access(request_api_key, client_ip):
                 return Response(status_code=status.HTTP_401_UNAUTHORIZED, content="Unauthorized")
             response: Response = await call_next(request)
             return response
         except InvalidToken:
-            return Response(status_code=status.HTTP_401_UNAUTHORIZED,content='Unauthorized')
+            return Response(status_code=status.HTTP_401_UNAUTHORIZED, content='Unauthorized')
 
     @InjectInMethod
-    def inject_middleware(self, securityService: SecurityService):
+    def inject_middleware(self, securityService: SecurityService, configService: ConfigService):
         self.securityService = securityService
+        self.configService = configService
 
-class AnalyticsMiddleware(MiddleWare,InjectableMiddlewareInterface):
+
+class AnalyticsMiddleware(MiddleWare, InjectableMiddlewareInterface):
     ...
 
 
-
-class JWTAuthMiddleware(MiddleWare,InjectableMiddlewareInterface):
+class JWTAuthMiddleware(MiddleWare, InjectableMiddlewareInterface):
     def __init__(self, app, dispatch=None) -> None:
         MiddleWare.__init__(self, app, dispatch)
         InjectableMiddlewareInterface.__init__(self)
 
     @InjectInMethod
-    def inject_middleware(self,jwtService: JWTAuthService):
+    def inject_middleware(self, jwtService: JWTAuthService):
         self.jwtService = jwtService
-       
+
     async def dispatch(self,  request: Request, call_next: Callable[..., Response]):
         token = get_bearer_token_from_request(request)
         client_ip = get_client_ip(request)
-        authPermission: AuthPermission = self.jwtService.verify_permission(token,client_ip)
+        authPermission: AuthPermission = self.jwtService.verify_permission(
+            token, client_ip)
         request.state.authPermission = authPermission
         return await call_next(request)
 
@@ -82,4 +91,4 @@ class MiddlewarePriority(Enum):
     PROCESS_TIME = 1
     ANALYTICS = 2
     SECURITY = 3
-    AUTH=4
+    AUTH = 4
