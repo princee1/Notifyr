12d7fe8d807129c7443dcbfcd69e247097b2b38e
princeey1 <prince-david.madzou@polymtl.ca>
2025-01-21 15:13:09 -0500

    feat: implement AuthPermissionPipe and enhance token issuance and refresh endpoints in AdminRessource
    fix: fix filter inject because we might need the args

--- app/ressources/admin_ressource.py
+++ app/ressources/admin_ressource.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Annotated, Any, Optional
+from typing import Annotated, Any, List, Optional
 from fastapi import Depends, Header, Request, Response,HTTPException,status
 from fastapi.responses import JSONResponse
 from services.assets_service import AssetService
@@ -7,11 +7,12 @@ from services.security_service import JWTAuthService,SecurityService
 from services.config_service import ConfigService
 from utils.dependencies import get_admin_token, get_auth_permission, get_bearer_token, get_client_ip
 from container import InjectInMethod,Get
-from definition._ressource import Guard, UseGuard, UseHandler, UsePermission,BaseRessource,HTTPMethod,Ressource
+from definition._ressource import Guard, UseGuard, UseHandler, UsePermission,BaseRessource,HTTPMethod,Ressource, UsePipe
 from decorators.permissions import JWTRouteHTTPPermission
 from classes.permission import AuthPermission,RoutePermission,AssetsPermission
 from pydantic import BaseModel, RootModel,field_validator
 from decorators.handlers import ServiceAvailabilityHandler
+from decorators.pipes import AuthPermissionPipe
 from utils.validation import ipv4_validator
 
 
@@ -60,16 +61,29 @@ class AdminRessource(BaseRessource):
 
     @UseHandler(ServiceAvailabilityHandler)
     @BaseRessource.HTTPRoute('/issue-auth/',methods=[HTTPMethod.GET])
-    def issue_token(self,authModel:AuthPermissionModel,authPermission=Depends(get_auth_permission)):
+    def issue_auth_token(self,authModel:AuthPermissionModel | List[AuthPermissionModel],authPermission=Depends(get_auth_permission)):
         self.jwtAuthService.pingService()
+        authModel:list[AuthPermissionModel] = authModel if isinstance(authModel,list) else [authModel]
+        temp = self._create_tokens(authModel)
+        return JSONResponse(status_code=status.HTTP_200_OK,content={"tokens":temp,"message":"Tokens successfully issued"})
 
-        issued_for = authModel.issued_for
-        allowed_routes = authModel.allowed_routes
-        #allowed_assets = authModel.allowed_assets
-        # TODO validation
-        api_token  = self.securityService.generate_custom_api_key(issued_for)
-        auth_token = self.jwtAuthService.encode_auth_token(allowed_routes,issued_for)
-        return JSONResponse(status_code=status.HTTP_200_OK,content={"api_token":api_token,"auth_token":auth_token})
+    @UseHandler(ServiceAvailabilityHandler)
+    @UsePipe(AuthPermissionPipe)
+    @BaseRessource.HTTPRoute('/refresh-auth/',methods=[HTTPMethod.GET,HTTPMethod.POST])
+    def refresh_auth_token(self,tokens:str |list[str], authPermission=Depends(get_auth_permission)):
+        self.jwtAuthService.pingService()
+        tokens:list[AuthPermission] = tokens if isinstance(tokens,list) else [tokens]
+        tokens = self._create_tokens(tokens)
+        return JSONResponse(status_code=status.HTTP_200_OK,content={'tokens':tokens ,"message":"Tokens successfully invalidated"})
     
 
-    
+    def _create_tokens(self,tokens):
+        temp ={}
+        for token in tokens:
+            issued_for = token['issued_for']
+            allowed_routes = token['allowed_routes']
+            # allowed_assets = token['allowed_assets']
+            api_token  = self.securityService.generate_custom_api_key(issued_for)
+            auth_token = self.jwtAuthService.encode_auth_token(allowed_routes,issued_for)
+            temp[issued_for]={"api_token":api_token,"auth_token":auth_token}
+        return temp 
