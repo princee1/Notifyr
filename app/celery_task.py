e0bb75c8d06eac33dad433f7f0b48d99b7828ae2
princeey1 <prince-david.madzou@polymtl.ca>
2025-01-22 13:04:09 -0500

    feat: add CeleryService for task management and update autodiscover_tasks reference

--- app/celery_task.py
+++ app/celery_task.py
@@ -5,9 +5,7 @@ celery_app = Celery('celery_app',
                 broker='redis://localhost/0'
             )
 celery_app.conf.update(task_serializer='pickle', accept_content=['pickle'])
-celery_app.autodiscover_tasks(['app.services'], related_name='email_service')
-
-
+celery_app.autodiscover_tasks(['app.services'], related_name='celery_service')
 
 
 # app.conf.update(
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -11,4 +11,5 @@ from .stats_service import *
 from .support_service import *
 from .throttling_service import *
 from .model_service import *
+from .celery_service import *
 from .twilio_service import *
