c22585ac3f279f1c830ffddc6c0e8246d741c75e
princeey1 <prince-david.madzou@polymtl.ca>
2025-02-14 23:46:26 -0500

    feat: integrate PingService into chat, redis, sms, and voice resources

--- app/ressources/redis_backend_ressource.py
+++ app/ressources/redis_backend_ressource.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from app.container import Get, InjectInMethod
 from app.decorators.handlers import CeleryTaskHandler, ServiceAvailabilityHandler, WebSocketHandler
 from app.decorators.permissions import JWTRouteHTTPPermission
-from app.definition._ressource import BaseHTTPRessource, HTTPMethod, HTTPRessource, UseHandler, UsePermission, UsePipe, UseRoles
+from app.definition._ressource import BaseHTTPRessource, HTTPMethod, HTTPRessource, PingService, UseHandler, UsePermission, UsePipe, UseRoles
 from app.services.celery_service import CeleryService
 from app.services.config_service import ConfigService
 from app.services.security_service import JWTAuthService
@@ -20,6 +20,7 @@ REDIS_PREFIX = 'redis'
 @UseRoles([Role.REDIS])
 @UsePermission(JWTRouteHTTPPermission)
 @UseHandler(ServiceAvailabilityHandler)
+@PingService([CeleryService])
 @HTTPRessource(prefix=REDIS_PREFIX,websockets=[RedisBackendWebSocket])
 class RedisBackendRessource(BaseHTTPRessource):
     
@@ -33,41 +34,34 @@ class RedisBackendRessource(BaseHTTPRessource):
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.Get('/task/{task_id}')
     def check_task(self,task_id:str,authPermission=Depends(get_auth_permission)):
-        self.celeryService.pingService()
         return self.celeryService.seek_result(task_id)
-        
 
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.Delete('/task/{task_id}')
     def cancel_task(self,task_id:str,authPermission=Depends(get_auth_permission)):
-        self.celeryService.pingService()
         self.celeryService.cancel_task(task_id)
 
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.Get('/schedule/{schedule_id}')
     def check_schedule(self,schedule_id:str,authPermission=Depends(get_auth_permission)):
-        self.celeryService.pingService()
         return self.celeryService.seek_schedule(schedule_id)
         
 
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.Delete('/schedule/{schedule_id}')
     def delete_schedule(self,schedule_id:str,authPermission=Depends(get_auth_permission)):
-        self.celeryService.pingService()
         self.celeryService.delete_schedule(schedule_id)
         
-
+    @PingService([JWTAuthService])
     @UseHandler(WebSocketHandler)
     @BaseHTTPRessource.Get('/create-permission/{ws_path}',)
     def invoke_notify_permission(self, ws_path:str, authPermission=Depends(get_auth_permission)):
-        self.celeryService.pingService()
-        self.jwtAuthService.pingService()
         self._check_ws_path(ws_path)
 
         redis_run_id = self.websockets[RedisBackendWebSocket.__name__].run_id
         token = self.jwtAuthService.encode_ws_token(redis_run_id,REDIS_EXPIRATION)
         return JSONResponse(status_code=status.HTTP_201_CREATED,content={
-            'chat-token':token,
+            'redis-token':token,
         })
 
 
