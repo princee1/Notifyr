cc44fb35c761b8a55138468574d6f7c47f050088
princeey1 <prince-david.madzou@polymtl.ca>
2025-02-20 18:33:20 -0500

    feat: add Twilio role and handler; update SMS and Voice resources for Twilio integration

--- app/ressources/sms_ressource.py
+++ app/ressources/sms_ressource.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from app.classes.auth_permission import Role
 from app.classes.celery import SchedulerModel
 from app.decorators.guards import CeleryTaskGuard
-from app.decorators.handlers import CeleryTaskHandler, ServiceAvailabilityHandler
+from app.decorators.handlers import CeleryTaskHandler, ServiceAvailabilityHandler, TwilioHandler
 from app.decorators.permissions import JWTAssetPermission,JWTRouteHTTPPermission
 from app.decorators.pipes import CeleryTaskPipe, TemplateParamsPipe
 from app.definition._ressource import HTTPRessource, PingService, UseGuard, UseLimiter, UsePermission, BaseHTTPRessource, UseHandler, UsePipe, UseRoles
@@ -12,7 +12,7 @@ from app.container import InjectInMethod, InjectInFunction
 from app.services.assets_service import AssetService
 from app.services.chat_service import ChatService
 from app.services.contacts_service import ContactsService
-from app.services.twilio_service import SMSService, verify_twilio_token
+from app.services.twilio_service import SMSService
 from app.utils.dependencies import get_auth_permission
 
 
@@ -20,21 +20,25 @@ SMS_ONGOING_PREFIX = 'sms-ongoing'
 
 class OnGoingSMSModel(BaseModel):
     ...
-class SMSTemplateSchedulerModel(SchedulerModel):
+
+class SMSSchedulerModel(SchedulerModel):
     content: Any # TODO
 
 
+@UseRoles([Role.TWILIO])
 @PingService([SMSService])
-@UseHandler(ServiceAvailabilityHandler)
+@UseHandler(ServiceAvailabilityHandler,TwilioHandler)
 @UsePermission(JWTRouteHTTPPermission)
 @HTTPRessource(SMS_ONGOING_PREFIX)
 class OnGoingSMSRessource(BaseHTTPRessource):
     @InjectInMethod
     def __init__(self, smsService: SMSService,assetService:AssetService,chatService:ChatService) -> None:
         self.smsService: SMSService = smsService
-        super().__init__(dependencies=[Depends(self.smsService.verify_twilio_token)])
         self.assetService: AssetService = assetService
         self.chatService: ChatService = chatService
+
+        super().__init__(dependencies=[Depends(self.smsService.verify_twilio_token)])
+
         
     @UseRoles([Role.MFA_OTP])
     @BaseHTTPRessource.HTTPRoute('/otp/')
@@ -51,22 +55,26 @@ class OnGoingSMSRessource(BaseHTTPRessource):
     @UseGuard(CeleryTaskGuard(['']))
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.HTTPRoute('/simple/')
-    def sms_simple_message(self,scheduler: SMSTemplateSchedulerModel,authPermission=Depends(get_auth_permission)):
+    def sms_simple_message(self,scheduler: SMSSchedulerModel,authPermission=Depends(get_auth_permission)):
         pass
     
     @UseHandler(CeleryTaskHandler)
     @UseRoles([Role.RELAY])
     @UseGuard(CeleryTaskGuard(['']))
     @UsePipe(TemplateParamsPipe('sms'),CeleryTaskPipe)
-    @UsePermission(JWTAssetPermission('html'))
+    @UsePermission(JWTAssetPermission('sms'))
     @BaseHTTPRessource.HTTPRoute('/template/{template}')
-    def sms_template(self,template:str,scheduler: SMSTemplateSchedulerModel,authPermission=Depends(get_auth_permission)):
+    def sms_template(self,template:str,scheduler: SMSSchedulerModel,authPermission=Depends(get_auth_permission)):
         ...
 
 
 
 SMS_INCOMING_PREFIX = "sms-incoming"
 
+@UseRoles([Role.TWILIO])
+@PingService([SMSService])
+@UseHandler(ServiceAvailabilityHandler,TwilioHandler)
+@UsePermission(JWTRouteHTTPPermission)
 @HTTPRessource(SMS_INCOMING_PREFIX )
 class IncomingSMSRessource(BaseHTTPRessource):
     @InjectInMethod
@@ -77,13 +85,18 @@ class IncomingSMSRessource(BaseHTTPRessource):
         super().__init__(dependencies=[Depends(self.smsService.verify_twilio_token)])
 
     @BaseHTTPRessource.HTTPRoute('/menu/',methods=['POST'])
-    def sms_menu(self,authPermission=Depends(get_auth_permission)):
+    async def sms_menu(self,authPermission=Depends(get_auth_permission)):
         pass
     
+    @UseRoles([Role.CHAT])
     @BaseHTTPRessource.HTTPRoute('/live-chat/',methods=['POST'])
-    def sms_chat(self,authPermission=Depends(get_auth_permission)):
+    async def sms_chat(self,authPermission=Depends(get_auth_permission)):
         pass
 
     @BaseHTTPRessource.HTTPRoute('/automate-response/',methods=['POST'])
-    def sms_automated(self,authPermission=Depends(get_auth_permission)):
+    async def sms_automated(self,authPermission=Depends(get_auth_permission)):
+        pass
+    
+    @BaseHTTPRessource.HTTPRoute('/handler_fail/',methods=['POST'])
+    async def sms_primary_handler_fail(self,authPermission=Depends(get_auth_permission)):
         pass
--- app/ressources/voice_ressource.py
+++ app/ressources/voice_ressource.py
@@ -1,80 +1,104 @@
+from typing import Any
 from fastapi import Depends
 from app.classes.auth_permission import Role
-from app.decorators.guards import ContactsGuard
-from app.decorators.handlers import ServiceAvailabilityHandler
-from app.decorators.permissions import JWTRouteHTTPPermission
+from app.classes.celery import SchedulerModel
+from app.decorators.guards import CeleryTaskGuard, ContactsGuard
+from app.decorators.handlers import CeleryTaskHandler, ServiceAvailabilityHandler, TemplateHandler, TwilioHandler
+from app.decorators.permissions import JWTAssetPermission, JWTRouteHTTPPermission
+from app.decorators.pipes import CeleryTaskPipe, TemplateParamsPipe
+from app.services.celery_service import CeleryService
 from app.services.chat_service import ChatService
 from app.services.contacts_service import ContactsService
 from app.services.twilio_service import VoiceService
-from app.definition._ressource import BaseHTTPRessource, BaseHTTPRessource, HTTPRessource, PingService, UseGuard, UseHandler, UsePermission, UseRoles
-from app.container import InjectInMethod, InjectInFunction
+from app.definition._ressource import BaseHTTPRessource, BaseHTTPRessource, HTTPRessource, PingService, UseGuard, UseHandler, UsePermission, UsePipe, UseRoles
+from app.container import Get, InjectInMethod, InjectInFunction
+from app.utils.dependencies import get_auth_permission
 
 
 CALL_ONGOING_PREFIX = 'call-ongoing'
 
+class VoiceSchedulerModel(SchedulerModel):
+    content: Any # TODO
+
+@UseRoles([Role.TWILIO])
 @PingService([VoiceService])
-@UseHandler(ServiceAvailabilityHandler)
+@UseHandler(ServiceAvailabilityHandler,TwilioHandler)
 @UsePermission(JWTRouteHTTPPermission)
 @HTTPRessource(CALL_ONGOING_PREFIX)
 class OnGoingCallRessources(BaseHTTPRessource):
+
     @InjectInMethod
     def __init__(self, voiceService: VoiceService,chatService:ChatService,contactsService:ContactsService) -> None:
         self.voiceService = voiceService
-
-        super().__init__(dependencies=[Depends(self.voiceService.verify_twilio_token)])
         self.chatService = chatService
         self.contactsService = contactsService
+        self.celeryService: CeleryService = Get(CeleryService)
+
+        super().__init__(dependencies=[Depends(self.voiceService.verify_twilio_token)])
+
 
     @UseRoles([Role.MFA_OTP])
     @BaseHTTPRessource.HTTPRoute('/otp/',methods=['POST'])
     def voice_relay_otp(self,):
         pass
-
+    
+    @UsePermission(JWTAssetPermission('phone'))
+    @UseHandler(TemplateHandler,CeleryTaskHandler)
+    @UsePipe(TemplateParamsPipe('phone'),CeleryTaskPipe)
+    @UseGuard(CeleryTaskGuard(['']))
     @UseRoles([Role.RELAY])
     @BaseHTTPRessource.HTTPRoute('/template/{template}/',methods=['POST'])
-    def voice_template(self,template:str):
+    def voice_template(self,template:str,scheduler: VoiceSchedulerModel):
         ...
 
+    @UseRoles([Role.RELAY])
+    @UsePipe(CeleryTaskPipe)
+    @UseGuard(CeleryTaskGuard(['']))
+    @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.HTTPRoute('/custom',methods=['POST'])
-    def voice_custom(self,):
+    def voice_custom(self,scheduler: VoiceSchedulerModel):
         ...
     
     @UseRoles([Role.MFA_OTP])
     @UseGuard(ContactsGuard)
     @BaseHTTPRessource.HTTPRoute('/authenticate/',methods=['GET'])
-    def voice_authenticate(self):
+    async def voice_authenticate(self):
         ...
     
 
-
-
-
 CALL_INCOMING_PREFIX = "call-incoming"
 
-@UseRoles([Role.CHAT])
+@UseRoles([Role.TWILIO])
 @PingService([VoiceService])
-@UseHandler(ServiceAvailabilityHandler)
+@UseHandler(ServiceAvailabilityHandler,TwilioHandler)
 @UsePermission(JWTRouteHTTPPermission)
 @HTTPRessource(CALL_INCOMING_PREFIX)
 class IncomingCallRessources(BaseHTTPRessource):
     @InjectInMethod
     def __init__(self, voiceService: VoiceService,chatService:ChatService,contactsService:ContactsService) -> None:
         self.voiceService = voiceService
-        super().__init__(dependencies=[Depends(self.voiceService.verify_twilio_token)])
         self.chatService = chatService
         self.contactsService = contactsService
+        super().__init__(dependencies=[Depends(self.voiceService.verify_twilio_token)])
 
 
     @BaseHTTPRessource.HTTPRoute('/menu/')
-    def voice_menu(self):
+    async def voice_menu(self,authPermission=Depends(get_auth_permission)):
         ...
     
+    @UseRoles([Role.CHAT])
     @BaseHTTPRessource.HTTPRoute('/live-chat/')
-    def voice_live_chat(self,):
+    async def voice_live_chat(self,authPermission=Depends(get_auth_permission)):
         pass
-
 
     @BaseHTTPRessource.HTTPRoute('/automate-response/')
-    def voice_automate_response(self,):
+    async def voice_automate_response(self,authPermission=Depends(get_auth_permission)):
         pass
 
+    @BaseHTTPRessource.HTTPRoute('/handler_fail/',methods=['POST'])
+    async def voice_primary_handler_fail(self,authPermission=Depends(get_auth_permission)):
+        pass
+
+    @BaseHTTPRessource.HTTPRoute('/status/',methods=['POST'])
+    async def voice_call_status_changes(self,authPermission=Depends(get_auth_permission)):
+        ...
