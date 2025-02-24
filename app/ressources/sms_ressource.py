7c572c3e66cfd9d032285b600c7e922d89ee57e8
princeey1 <prince-david.madzou@polymtl.ca>
2025-02-23 22:17:33 -0500

    feat: enhance SMS resource and Twilio service with new models and methods for OTP and custom SMS handling

--- app/ressources/sms_ressource.py
+++ app/ressources/sms_ressource.py
@@ -1,17 +1,21 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, List
 from fastapi import Depends, Header
 from pydantic import BaseModel
 from app.classes.auth_permission import Role
-from app.classes.celery import SchedulerModel
+from app.classes.celery import SchedulerModel, TaskType
+from app.classes.template import SMSTemplate
 from app.decorators.guards import CeleryTaskGuard
 from app.decorators.handlers import CeleryTaskHandler, ServiceAvailabilityHandler, TwilioHandler
 from app.decorators.permissions import JWTAssetPermission,JWTRouteHTTPPermission
-from app.decorators.pipes import CeleryTaskPipe, TemplateParamsPipe
+from app.decorators.pipes import CeleryTaskPipe, TemplateParamsPipe, TwilioFromPipe
 from app.definition._ressource import HTTPRessource, PingService, UseGuard, UseLimiter, UsePermission, BaseHTTPRessource, UseHandler, UsePipe, UseRoles
 from app.container import InjectInMethod, InjectInFunction
 from app.models.otp_model import OTPModel
+from app.models.sms_model import OnGoingBaseSMSModel, OnGoingSMSModel, OnGoingTemplateSMSModel
 from app.services.assets_service import AssetService
+from app.services.celery_service import CeleryService
 from app.services.chat_service import ChatService
+from app.services.config_service import ConfigService
 from app.services.contacts_service import ContactsService
 from app.services.twilio_service import SMSService
 from app.utils.dependencies import get_auth_permission
@@ -19,12 +23,11 @@ from app.utils.dependencies import get_auth_permission
 
 SMS_ONGOING_PREFIX = 'sms-ongoing'
 
-class OnGoingSMSModel(BaseModel):
-    ...
+class SMSCustomSchedulerModel(SchedulerModel):
+    content: OnGoingSMSModel
 
-class SMSSchedulerModel(SchedulerModel):
-    content: Any # TODO
-
+class SMSTemplateSchedulerModel(SchedulerModel):
+    content: OnGoingTemplateSMSModel
 
 @PingService([SMSService])
 @UseHandler(ServiceAvailabilityHandler,TwilioHandler)
@@ -32,33 +35,47 @@ class SMSSchedulerModel(SchedulerModel):
 @HTTPRessource(SMS_ONGOING_PREFIX)
 class OnGoingSMSRessource(BaseHTTPRessource):
     @InjectInMethod
-    def __init__(self, smsService: SMSService,chatService:ChatService,contactService:ContactsService) -> None:
+    def __init__(self, smsService: SMSService,chatService:ChatService,contactService:ContactsService,configService:ConfigService,celeryService:CeleryService) -> None:
         super().__init__()
         self.smsService: SMSService = smsService
         self.chatService: ChatService = chatService
         self.contactService: ContactsService = contactService
-        
+        self.configService:ConfigService = configService
+        self.celeryService: CeleryService = celeryService
+
+    @UsePipe(TwilioFromPipe)
     @UseRoles([Role.MFA_OTP])
     @BaseHTTPRessource.HTTPRoute('/otp/')
     def sms_relay_otp(self,otpModel:OTPModel,authPermission=Depends(get_auth_permission)):
-        pass
+        return self.smsService.send_otp(otpModel)
     
     @UseRoles([Role.RELAY])    
-    @UsePipe(CeleryTaskPipe)
-    @UseGuard(CeleryTaskGuard(['']))
+    @UsePipe(CeleryTaskPipe,TwilioFromPipe)
+    @UseGuard(CeleryTaskGuard(task_names=['task_send_custom_sms']))
     @UseHandler(CeleryTaskHandler)
     @BaseHTTPRessource.HTTPRoute('/custom/')
-    def sms_simple_message(self,scheduler: SMSSchedulerModel,authPermission=Depends(get_auth_permission)):
-        pass
+    def sms_simple_message(self,scheduler: SMSCustomSchedulerModel,authPermission=Depends(get_auth_permission)):
+        message = scheduler.content.model_dump()
+        if scheduler.task_type == TaskType.NOW:
+            return self.smsService.send_template_sms(message)
+        return self.celeryService.trigger_task_from_scheduler(scheduler,message)
+
     
     @UseHandler(CeleryTaskHandler)
     @UseRoles([Role.RELAY])
-    @UseGuard(CeleryTaskGuard(['']))
-    @UsePipe(TemplateParamsPipe('sms'),CeleryTaskPipe)
+    @UseGuard(CeleryTaskGuard([' task_send_template_sms']))
+    @UsePipe(TemplateParamsPipe('sms'),CeleryTaskPipe,TwilioFromPipe)
     @UsePermission(JWTAssetPermission('sms'))
     @BaseHTTPRessource.HTTPRoute('/template/{template}')
-    def sms_template(self,template:str,scheduler: SMSSchedulerModel,authPermission=Depends(get_auth_permission)):
-        ...
+    def sms_template(self,template:str,scheduler: SMSTemplateSchedulerModel,authPermission=Depends(get_auth_permission)):
+        sms_data = scheduler.content
+        smsTemplate:SMSTemplate = self.assetService.sms[template]
+        _,result=smsTemplate.build(self.configService.ASSET_LANG,sms_data.data)
+        message = {'body':result,'to':sms_data.to,'from_':sms_data.from_}
+        
+        if scheduler.task_type == TaskType.NOW:
+            return self.smsService.send_template_sms(message)
+        return self.celeryService.trigger_task_from_scheduler(scheduler,message)
 
 
 
--- app/services/twilio_service.py
+++ app/services/twilio_service.py
@@ -4,12 +4,29 @@ https://www.youtube.com/watch?v=-AChTCBoTUM
 
 from typing import Annotated
 from fastapi import HTTPException, Header, Request
+from app.classes.template import SMSTemplate
 from app.definition import _service
+from app.models.otp_model import OTPModel
+from app.services.assets_service import AssetService
 from app.services.logger_service import LoggerService
 from .config_service import ConfigService
 from app.utils.helper import b64_encode
-#from twilio.rest import Client
+from twilio.request_validator import RequestValidator
+from twilio.rest import Client
+from app.utils.validation import phone_number_validator
+from app.classes.twilio import TwilioPhoneNumberParseError
+from datetime import timedelta
 
+letter_to_number = {
+    'A': '2', 'B': '2', 'C': '2',
+    'D': '3', 'E': '3', 'F': '3',
+    'G': '4', 'H': '4', 'I': '4',
+    'J': '5', 'K': '5', 'L': '5',
+    'M': '6', 'N': '6', 'O': '6',
+    'P': '7', 'Q': '7', 'R': '7', 'S': '7',
+    'T': '8', 'U': '8', 'V': '8',
+    'W': '9', 'X': '9', 'Y': '9', 'Z': '9'
+}
 
 
 @_service.ServiceClass
@@ -19,19 +36,36 @@ class TwilioService(_service.Service):
         self.configService = configService
 
     def build(self):
-        # self.client = Client(self.configService.TWILIO_ACCOUNT_SID,
-        #                      self.configService.TWILIO_AUTH_TOKEN)
-        return super().build()
-    
-    
+        self.client = Client(self.configService.TWILIO_ACCOUNT_SID,
+                             self.configService.TWILIO_AUTH_TOKEN)
+
+    def parse_to_phone_format(self, phone_number: str) -> str:
+        phone_number = phone_number.upper()
+        converted_number = ''.join(letter_to_number.get(
+            char, char) for char in phone_number)
+
+        cleaned_number = ''.join(filter(str.isdigit, converted_number))
+
+        if not cleaned_number.startswith('1'):
+            cleaned_number = '1' + cleaned_number
+
+        formatted_number = f"+{cleaned_number}"
+
+        if not phone_number_validator(formatted_number):
+            raise TwilioPhoneNumberParseError(formatted_number)
+
+        return formatted_number
+
+
 @_service.AbstractServiceClass
 class BaseTwilioCommunication(_service.Service):
-    def __init__(self, configService: ConfigService, twilioService: TwilioService) -> None:
+    def __init__(self, configService: ConfigService, twilioService: TwilioService, assetService: AssetService) -> None:
         super().__init__()
         self.configService = configService
         self.twilioService = twilioService
-    
-    async def verify_twilio_token(self,request:Request):
+        self.assetService = assetService
+
+    async def verify_twilio_token(self, request: Request):
         twilio_signature = request.headers.get("X-Twilio-Signature", "")
 
         full_url = str(request.url)
@@ -41,8 +75,8 @@ class BaseTwilioCommunication(_service.Service):
 
         validator = RequestValidator(self.configService.TWILIO_AUTH_TOKEN)
         if not validator.validate(full_url, params, twilio_signature):
-            raise HTTPException(status_code=403, detail="Invalid Twilio Signature")
-        ...
+            raise HTTPException(
+                status_code=403, detail="Invalid Twilio Signature")
 
 
 @_service.ServiceClass
@@ -50,12 +84,38 @@ class SMSService(BaseTwilioCommunication):
 
     def __init__(self, configService: ConfigService, twilioService: TwilioService):
         super().__init__(configService, twilioService)
+        self.status_callback = ...
+
+    def send_otp(self, otpModel: OTPModel):
+        company = otpModel.brand
+        otp = otpModel.otp
+        expiry = timedelta(seconds=otpModel.expiry).min
+        match otpModel.type:
+            case "login":
+                otp_phrase = f"Your login OTP for {company} is {otp}. It will expire in {expiry} minutes. Do not share it."
+
+            case "transaction":
+                otp_phrase = f"Your OTP for confirming your transaction is {otp}. This code will expire in {expiry} minutes."
+
+            case "mfa":
+                otp_phrase = f"Use this OTP ({otp}) to verify your login for {company}. This code expires in {expiry} minutes."
+
+            case "password_reset":
+                otp_phrase = f"Use {otp} to reset your password for {company}. This code is valid for {expiry} minutes."
+
+            case _:
+                otp_phrase = f"Your OTP code is: {otp}. Do not share this code with anyone. It expires soon."
+
+        return self.message.create(to=otpModel.to, status_callback=self.status_callback, from_=otpModel.from_, body=otp_phrase)
 
     def build(self):
-        #self.message = self.twilioService.client.messages
-        return super().build()
-        
-    pass
+        self.message = self.twilioService.client.messages
+
+    def send_custom_sms(self, messageData: dict):
+        return self.message.create(send_as_mms=True, status_callback=self.status_callback, **messageData)
+
+    def send_template_sms(self, message):
+        return self.message.create(send_as_mms=True, status_callback=self.status_callback, **message)
 
 
 @_service.ServiceClass
@@ -82,6 +142,6 @@ class SIPService(BaseTwilioCommunication):
 
 @_service.ServiceClass
 class WhatsAppService(BaseTwilioCommunication):
-    def __init__(self, configService: ConfigService, loggerService: LoggerService,twilioService: TwilioService):
-        super().__init__(configService,twilioService)
+    def __init__(self, configService: ConfigService, loggerService: LoggerService, twilioService: TwilioService):
+        super().__init__(configService, twilioService)
         self.loggerService = loggerService
