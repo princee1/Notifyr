e7cc8edab02231f39d4dbfeb7e6461ac4e3a361c
princeey1 <prince-david.madzou@polymtl.ca>
2025-03-12 18:17:41 -0400

    feat: enhance contacts schema with new domains, tables, and functions for improved data management

--- database/contacts.sql
+++ database/contacts.sql
@@ -1,38 +1,76 @@
 -- Active: 1740679093248@@127.0.0.1@5432@notifyr@contacts
-DROP SCHEMA IF  EXISTS contacts CASCADE;
+DROP SCHEMA IF EXISTS contacts CASCADE;
+
 CREATE SCHEMA contacts;
+
 SET search_path = contacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE DOMAIN Lang AS VARCHAR(15) CHECK (VALUE IN ('fr','en'))
+CREATE DOMAIN Lang AS VARCHAR(15) CHECK (VALUE IN ('fr', 'en'))
 
-CREATE DOMAIN SubscriptionStatus AS VARCHAR(20) CHECK (VALUE IN ('Active','Inactive'))
+CREATE DOMAIN SubscriptionStatus AS VARCHAR(20) CHECK (
+    VALUE IN ('Active', 'Inactive')
+)
+
+CREATE DOMAIN Status AS VARCHAR(20) CHECK (
+    VALUE IN (
+        'Active',
+        'Pending',
+        'Blacklist',
+        'Inactive'
+    )
+)
+
+CREATE DOMAIN Frequency AS VARCHAR(20) CHECK (
+    VALUE IN (
+        'weekly',
+        'bi_weekly',
+        'monthly',
+        'always'
+    )
+)
+
+CREATE DOMAIN ContentType AS VARCHAR(30) CHECK (
+    VALUE IN (
+        'newsletter',
+        'event',
+        'notification',
+        'promotion',
+        'update',
+        'other'
+    )
+)
 
 CREATE TABLE IF NOT EXISTS Contact (
-    contact_id  UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    first_name  VARCHAR(50) NOT NULL,
-    last_name   VARCHAR(50) NOT NULL,
-    email       VARCHAR(50) UNIQUE NOT NULL,
-    phone       VARCHAR(50) UNIQUE,
-    app_registered  BOOLEAN DEFAULT FALSE,
-    lang    Lang,
-    created_at  TIMESTAMPTZ DEFAULT NOW(),
-    updated_at  TIMESTAMPTZ DEFAULT NOW()
+    contact_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    phone VARCHAR(50) UNIQUE,
+    status Status DEFAULT 'Active',
+    app_registered BOOLEAN DEFAULT FALSE,
+    opt_in_code INT UNIQUE,
+    lang Lang DEFAULT 'en',
+    frequency Frequency DEFAULT 'always',
+    action_code TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    CONSTRAINT chk_opt_in_code CHECK (opt_in_code >= 100000 AND opt_in_code <= 999999)
 )
 
 CREATE TABLE IF NOT EXISTS SecurityContact (
-    security_id  UUID PRIMARY KEY  DEFAULT uuid_generate_v4(),
-    contact_id  UUID,
+    security_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    contact_id UUID,
     security_code TEXT DEFAULT NULL,
     security_code_salt VARCHAR(64) DEFAULT NULL,
     security_phrase TEXT DEFAULT NULL,
     security_phrase_salt VARCHAR(64) DEFAULT NULL,
     voice_embedding TEXT DEFAULT NULL,
     voice_embedding_salt VARCHAR(64) DEFAULT NULL,
-    created_at  TIMESTAMPTZ DEFAULT NOW(),
-    updated_at  TIMESTAMPTZ DEFAULT NOW(),
-    FOREIGN KEY (contact_id) REFERENCES Contact(contact_id) ON DELETE CASCADE ON UPDATE CASCADE
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (contact_id) REFERENCES Contact (contact_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE IF NOT EXISTS SubscriptionContact (
@@ -43,9 +81,87 @@ CREATE TABLE IF NOT EXISTS SubscriptionContact (
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW(),
     PRIMARY KEY (subscription_id),
-    FOREIGN KEY (contact_id) REFERENCES Contact(contact_id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (contact_id) REFERENCES Contact (contact_id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS Reason (
+    reason_id UUID DEFAULT uuid_generate_v4(),
+    reason_description TEXT DEFAULT NULL,
+    reason_name UNIQUE TEXT,
+    reason_count BIGINT DEFAULT 0,
+    PRIMARY KEY (reason_id)
+)
+
+CREATE TABLE IF NOT EXISTS SubsContent (
+    content_id UUID DEFAULT uuid_generate_v4(),
+    content_name UNIQUE VARCHAR(50),
+    content_description TEXT DEFAULT NULL,
+    content_type ContentType DEFAULT 'other'
+    PRIMARY KEY (content_id)
+)
+
+CREATE TABLE IF NOT EXISTS Subscription (
+    subs_id UUID UNIQUE DEFAULT uuid_generate_v4(),
+    contact_id UUID,
+    content_id UUID,
+    subs_status SubscriptionStatus DEFAULT 'Active',
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    PRIMARY KEY (
+        subs_id,
+        contact_id,
+        content_id
+    ) FOREIGN KEY (contact_id) REFERENCES Contact (contact_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (content_id) REFERENCES SubsContent (content_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 
--- 1-to-many Subscriptions + manage subscription
+CREATE OR REPLACE FUNCTION update_reason(VARCHAR(50)) RETURNS VOID AS $$ BEGIN
+RETURN QUERY
+
+SELECT pg_advisory_xact_lock(hashtext($1));
+INSERT INTO 
+    contacts.Reason (reason_name, reason_count)
+VALUES ($1, 1)
+ON CONFLICT (reason) DO
+UPDATE contacts.Reason
+SET 
+    reason_count = reason_count + 1
+WHERE 
+    reason_name = $1;
+END;
+
+% % LANGUAGE PLPGSQL SECURITY DEFINER ;
+
+
+
+CREATE OR REPLACE FUNCTION reset_reason(VARCHAR(50)) RETURNS VOID AS $$ BEGIN
+RETURN QUERY
+
+SELECT pg_advisory_xact_lock(hashtext ($1));
+
+INSERT INTO
+    contacts.Reason (reason_name, reason_count)
+VALUES ($1, 0)
+ON CONFLICT (reason) DO
+UPDATE contacts.Reason
+SET
+    contacts.reason_count = 0
+WHERE
+    contacts.reason_name = $1;
+
+END;
+
+% % LANGUAGE PLPGSQL SECURITY DEFINER ;
+
+
+-- CREATE OR REPLACE FUNCTION check_user_status(UUID,VARCHAR(50)) RETURNS BOOlEAN AS $$ BEGIN
+-- RETURN QUERY
+
+-- END;
+
+
+
+
+
 -- users Views
