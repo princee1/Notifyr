469fc19b99a45408227b1049a688e265b12add88
princeey1 <prince-david.madzou@polymtl.ca>
2025-03-16 12:54:53 -0400

    feat: add pgAgent job setup for deleting expired SubsContent entries with daily schedule

--- database/security.sql
+++ database/security.sql
@@ -10,3 +10,27 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 -- NOTE or create a schedule for each row using a Trigger
+
+-- -- Create the pgAgent job
+-- INSERT INTO pgagent.pga_job (
+--     jobid, jobname, jobdesc, jobhostagent, jobenabled
+-- ) VALUES (
+--     DEFAULT, 'Delete Expired SubsContent', 'Deletes expired SubsContent entries', '', true
+-- );
+
+-- -- Get the job ID of the newly created job
+-- SELECT currval('pgagent.pga_job_jobid_seq');
+
+-- -- Create a schedule for the job (run daily at midnight)
+-- INSERT INTO pgagent.pga_schedule (
+--     jscjobid, jscname, jscdesc, jscenabled, jscweekdays, jscmonthdays, jsctime
+-- ) VALUES (
+--     <job_id>, 'Daily at Midnight', 'Runs daily at midnight', true, '1111111', '1', '00:00'
+-- );
+
+-- -- Create a step for the job to call the function
+-- INSERT INTO pgagent.pga_jobstep (
+--     jstjobid, jstname, jstdesc, jstenabled, jstkind, jstcode, jstdbname
+-- ) VALUES (
+--     <job_id>, 'Call delete_expired_subscontent', 'Calls the delete_expired_subscontent function', true, 'sql', 'SELECT delete_expired_subscontent();', 'notifyr'
+-- );
