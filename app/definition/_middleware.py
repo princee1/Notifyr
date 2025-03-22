04f15d02c5b78a553d108fabd052df9bd401f454
princeey1 <prince-david.madzou@polymtl.ca>
2025-03-22 11:13:17 -0400

    feat: enhance middleware path matching with include and exclude patterns

--- app/definition/_middleware.py
+++ app/definition/_middleware.py
@@ -1,9 +1,11 @@
 import functools
-from typing import Callable
+from typing import Callable, Literal
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction
 from starlette.datastructures import MutableHeaders,Headers
 from enum import Enum
+import asyncio
+from fnmatch import fnmatch
 
 MIDDLEWARE: dict[str, type] = {}
 class MiddlewarePriority(Enum):
@@ -21,8 +23,20 @@ class MiddleWare(BaseHTTPMiddleware):
     def __init_subclass__(cls: type) -> None:
         MIDDLEWARE[cls.__name__] = cls
         #setattr(cls,'priority',None)
-    
-def ApplyOn(paths:list[str],exclude:list[str]=[],methods:list[str]=[],options:list[Callable[...,bool]]=[]   ):
+
+
+def path_matcher(paths: list[str], url: str) -> bool:
+    if not paths:
+        return True
+    return any(fnmatch(url, pattern) for pattern in paths)
+
+def exclude_path_matcher(excludes: list[str], url: str) -> bool:
+    if not excludes:
+        return True
+    return any(fnmatch(url, pattern) for pattern in excludes)
+
+
+def ApplyOn(paths:list[str]=[],exclude:list[str]=[],methods:list[Literal['GET','POST','PATCH','PUT','DELETE','HEAD','OPTION']]=[],options:list[Callable[[Request],bool]]=[]   ):
     def decorator(func:Callable):
 
         @functools.wraps(func)
@@ -30,9 +44,23 @@ def ApplyOn(paths:list[str],exclude:list[str]=[],methods:list[str]=[],options:li
             base_url = str(request.base_url)
             url = str(request.url).replace(base_url,"")
 
-            if url==base_url:
-                return await func(self,request,call_next)
+            if not path_matcher(paths,url):
+                return await call_next(request)
 
-            return await call_next(request)
+            if not exclude_path_matcher(exclude,url):
+                return await call_next(request)
+            
+            if methods and request.method not in methods:
+                return await call_next(request)
+      
+            for option in options:
+                if asyncio.iscoroutinefunction(option):
+                    if not await option(request):
+                        return await call_next(request)            
+                else:
+                    if not option(request):
+                        return await call_next(request)
+            
+            return await func(self,request,call_next)
         return wrapper
     return decorator
