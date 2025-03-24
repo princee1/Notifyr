4292cfa49b7e51b40de3717211d5f2b29abbee7c
princeey1 <prince-david.madzou@polymtl.ca>
2025-03-23 21:44:02 -0400

    feat: add JSON serialization properties to GroupClientORM and ClientORM, and update field constraints

--- app/models/security_model.py
+++ app/models/security_model.py
@@ -22,13 +22,22 @@ class GroupClientORM(models.Model):
         schema = SCHEMA
         table = "groupclient"
 
+    @property
+    def to_json(self):
+        return {
+            "group_id": str(self.group_id),
+            "group_name": self.group_name,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+
 class ClientORM(models.Model):
     client_id = fields.UUIDField(pk=True, default=uuid.uuid4)
     client_name = fields.CharField(max_length=200, unique=True, null=True)
-    client_scope = fields.CharEnumField(enum_type=Scope, default=Scope.SoloDolo)
+    client_scope = fields.CharEnumField(enum_type=Scope, default=Scope.SoloDolo, max_length=25)
     authenticated = fields.BooleanField(default=False)
-    client_type = fields.CharEnumField(enum_type=ClientType, default=ClientType.User)
-    issued_for = fields.CharField(max_length=50, null=False,unique=True)
+    client_type = fields.CharEnumField(enum_type=ClientType, default=ClientType.User, max_length=25)
+    issued_for = fields.CharField(max_length=50, null=False, unique=True)
     group = fields.ForeignKeyField("models.GroupClientORM", related_name="group", on_delete=fields.SET_NULL, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
@@ -37,12 +46,26 @@ class ClientORM(models.Model):
         schema = SCHEMA
         table = "client"
 
+    @property
+    def to_json(self):
+        return {
+            "client_id": str(self.client_id),
+            "client_name": self.client_name,
+            "client_scope": self.client_scope.value,
+            "authenticated": self.authenticated,
+            "client_type": self.client_type.value,
+            "issued_for": self.issued_for,
+            "group_id": str(self.group) if self.group else None,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+
 class ChallengeORM(models.Model):
     client = fields.OneToOneField("models.ClientORM", pk=True, related_name="challenge", on_delete=fields.CASCADE)
-    challenge_auth = fields.TextField()
+    challenge_auth = fields.TextField(generated=True)
     created_at_auth = fields.DatetimeField(auto_now_add=True)
     expired_at_auth = fields.DatetimeField(null=True)
-    challenge_refresh = fields.TextField()
+    challenge_refresh = fields.TextField(generated=True)
     created_at_refresh = fields.DatetimeField(auto_now_add=True)
     expired_at_refresh = fields.DatetimeField(null=True)
 
@@ -73,10 +96,13 @@ class GroupModel(BaseModel):
         return group_name.capitalize()
     
 
-ClientModelBase = pydantic_model_creator(ClientORM, name="ClientORM", exclude=('created_at', 'updated_at','client_id'))
+ClientModelBase = pydantic_model_creator(ClientORM, name="ClientORM", exclude=('created_at', 'updated_at','client_id',"authenticated","client_scope","group"))
 
 class ClientModel(ClientModelBase):
     
+    client_scope:Scope
+    group_id:str | None = None
+
     @model_validator(mode="after")
     def validate(self)->Self:
         if self.client_scope == Scope.Organization:
@@ -86,9 +112,6 @@ class ClientModel(ClientModelBase):
         if not ipv4_validator(self.issued_for):
             raise ValueError('Invalid ipv4 address')
         return self
-
-
-
 
 
 async def raw_revoke_challenges(client:ClientORM):
