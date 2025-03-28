ff68b17d7735d96eccf52de3d9bb725c0e25776e
princeey1 <prince-david.madzou@polymtl.ca>
2025-03-28 00:05:00 -0400

    feat: refactor authentication service to include base URL handling and improve token management

--- twilio/Notifyr-Auth/assets/contacts.private.js
+++ twilio/Notifyr-Auth/assets/contacts.private.js
@@ -1,8 +1,16 @@
 // retrieve the contacts code and wait for the user to authenticate if it works we pass to the other function
-
+const axios = require('axios')
 
 class Contact {
-    constructor() {
+    constructor(base_url) {
+        this.base_url = base_url
+    }
+
+    getContact(headers){
+
+    }
+
+    authenticate(headers){
     }
 
 
