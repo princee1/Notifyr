17828e6fc8f3851c7b672ef472b591199d8d1423
princeey1 <prince-david.madzou@polymtl.ca>
2025-07-13 23:18:30 -0400

    bug: not usable for now

--- app/depends/variables.py
+++ app/depends/variables.py
@@ -81,6 +81,6 @@ force_update: Callable[[Request],bool]=get_query_params('force',False,True,raise
 
 # ----------------------------------------------                                    ---------------------------------- #
 
-wait_timeout = Query(0, description="Time in seconds wait for the response", ge=0, le=60*3)
+wait_timeout_query = Query(0, description="Time in seconds wait for the response", ge=0, le=60*3)
 
 # ----------------------------------------------                                    ---------------------------------- #
