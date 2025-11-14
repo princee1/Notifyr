ee5b60eeeb9d63e5a65c9626e7a81b51370b306a
princeey1 <prince-david.madzou@polymtl.ca>
2025-11-14 14:21:34 -0500

    FEAT: created pacakged

--- app/services/webhook_service.py
+++ app/services/webhook_service.py
@@ -1,13 +1,16 @@
-from app.definition._service import Service, ServiceClass, ServiceStatus
+from app.definition._service import BaseService, Service, ServiceStatus
+from app.services.profile_service import ProfileService
+from app.services.reactive_service import ReactiveService
 from .config_service import ConfigService
 
-@ServiceClass
-class WebhookService(Service):
+@Service()
+class WebhookService(BaseService):
     
-    def __init__(self,configService:ConfigService):
+    def __init__(self,configService:ConfigService,profileService:ProfileService,reactiveService:ReactiveService):
         super().__init__()
-
         self.configService = configService
+        self.profileService = profileService
+        self.reactiveService = reactiveService
 
     
     def build(self):
